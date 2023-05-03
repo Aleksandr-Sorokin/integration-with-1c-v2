9afdd239f3251b354c7b7005cf9919acf264ad3a
Aleksandr <al-sorokin.yd@yandex.ru>
2023-05-03 22:45:12 +0700

    first commit

---
+++ src/main/java/ru/sorokin/integrationwith1cv2/IntegrationWith1cV2Application.java
@@ -0,0 +1,13 @@
+package ru.sorokin.integrationwith1cv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationWith1cV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationWith1cV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sorokin/integrationwith1cv2/IntegrationWith1cV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sorokin.integrationwith1cv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationWith1cV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
